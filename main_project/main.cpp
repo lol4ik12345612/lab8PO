2c9a6f61eebb4523c0391d87622cde2ad34a8a99
unknown <denisprosenko365@gmail.com>
2025-05-25 15:46:38 +0300

    Добавлены тестовые данные

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #7. Bank operation\n";
     cout << "Author: Denis Proschenko\n";
+    cout << "Group: 24ISIT\n";
     return 0;
 }
