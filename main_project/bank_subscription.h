abf4c709a8be97bc7f97d1c7fc0d5b9b51f5a045
unknown <denisprosenko365@gmail.com>
2025-05-25 15:26:34 +0300

    Добавлены структуры для хранения данных

---
+++ main_project/bank_subscription.h
@@ -0,0 +1,10 @@
+#include <string>
+
+struct BankOperation {
+    std::string date;       // формат: дд.мм.гг
+    std::string time;       // формат: чч:мм:сс
+    std::string type;       // "приход" или "расход"
+    std::string account;    // номер счёта
+    double amount;          // сумма
+    std::string purpose;    // назначение платежа
+};
